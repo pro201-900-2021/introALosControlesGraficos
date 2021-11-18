34f9d540ba03e3a2570b5b7354c3acf1d519863b
dotCero <lucero.inginfo@gmail.com>
2021-11-18 13:55:54 -0300

    Agregar archivos de proyecto.

---
+++ introALosControlesGraficos/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using introALosControlesGraficos.model;
+
+namespace introALosControlesGraficos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Persona> listaP;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            listaP = new List<Persona>();
+        }
+
+        private void buttonRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            string nombrePersona = textBoxNombre.Text;
+            string apellidoPersona = textBoxApellido.Text;
+            int edadPersona = int.Parse(textBoxEdad.Text);//Convert.ToInt32
+            Persona p = new Persona(nombrePersona, apellidoPersona, edadPersona);
+            listaP.Add(p);
+
+            textBoxNombre.Text = "";
+            textBoxApellido.Text = "";
+            textBoxEdad.Text = "";
+
+            textBoxNombre.Focus();
+
+            foreach(Persona pe in listaP)
+            {
+                Debug.WriteLine(pe.Nombre);
+                Debug.WriteLine(pe.Apellido);
+                Debug.WriteLine(pe.Edad);
+                Debug.WriteLine("***");
+                    
+            }
+        }
+
+    }
+}
---
+++ introALosControlesGraficos/model/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introALosControlesGraficos.model
+{
+    class Persona
+    {
+        private string _nombre;
+        private string _apellido;
+        private int _edad;
+
+        public Persona(string nombre, string apellido, int edad)
+        {
+            _nombre = nombre;
+            _apellido = apellido;
+            _edad = edad;
+        }
+
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellido { get => _apellido; set => _apellido = value; }
+        public int Edad { get => _edad; set => _edad = value; }
+    }
+}
