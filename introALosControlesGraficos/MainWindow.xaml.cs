2f8841150157e14dc5f36597421cf7ae62b03e7f
dotCero <lucero.inginfo@gmail.com>
2021-11-30 21:21:10 -0300

    Actualización a la intro a los controles gráficos, incluído el combobox, llenado y uso.

--- introALosControlesGraficos/MainWindow.xaml.cs
+++ introALosControlesGraficos/MainWindow.xaml.cs
@@ -24,13 +24,37 @@ namespace introALosControlesGraficos
     {
         List<Persona> listaP;
 
-        public MainWindow()
+        string[] listaProfesiones = new string[]
+        {
+            "Odontóloga/o",
+            "Ingeniera/o",
+            "Contador/a",
+            "Periodista"
+        };
+
+        List<Profesion> listaProfesiones2 = new List<Profesion>();
+
+        public MainWindow()//constructor
         {
             InitializeComponent();
             listaP = new List<Persona>();
+
+            listaProfesiones2.Add(new Profesion(1, "Odontoga/o", "Profesión de la salud dedicada a la salud bucal."));
+            listaProfesiones2.Add(new Profesion(2, "Ingeniera/o", "Profesión dedicada a ser dioses."));
+            listaProfesiones2.Add(new Profesion(3, "Contador", "Profesión dedicada a contar."));
+            listaProfesiones2.Add(new Profesion(4, "Periodista", "Profesión dedicada a narrar noticias."));
+
+            foreach(Profesion pr in listaProfesiones2)
+            {
+                Debug.WriteLine(pr);
+            }
+
+
+            //con esta Propiedad, podemos establecer el origen de datos de un combobox
+            comboBoxProfesion.ItemsSource = listaProfesiones2;
         }
 
-        private void buttonRegistrar_Click(object sender, RoutedEventArgs e)
+        private void buttonRegistrar_Click(object sender, RoutedEventArgs e)//funciones o métodos
         {
             string nombrePersona = textBoxNombre.Text;
             string apellidoPersona = textBoxApellido.Text;
@@ -54,5 +78,19 @@ namespace introALosControlesGraficos
             }
         }
 
+        private void buttonRegistrar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine("Estoy encima del botón");
+        }
+
+        private void comboBoxProfesion_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Esto me permite capturar el item seleccionado
+            //Debemos hacer un "casting" para que el valor que viene como 'object' sea transformado a la clase que necesitamos
+            //en este caso, profesión
+            Profesion itemSeleccionado = (Profesion)comboBoxProfesion.SelectedItem;
+
+            Debug.WriteLine(itemSeleccionado.Descripcion);
+        }
     }
 }
---
+++ introALosControlesGraficos/model/Profesion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introALosControlesGraficos.model
+{
+    class Profesion
+    {
+        private int id;
+        private string nombre;
+        private string descripcion;
+
+        public Profesion(int id, string nombre, string descripcion)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+
+        
+        public override string ToString()
+        {
+            return nombre;
+        }
+        
+    }
+}
